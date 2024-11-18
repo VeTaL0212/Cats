08795ca97563c7ae6b2d893c5df16bed1a00b0b0
Vitali <vitaly.u@mail.ru>
2024-11-18 20:24:14 +0300

    Теперь каждое изображение откроется в новом окне

--- Cataas.py
+++ Cataas.py
@@ -19,11 +19,15 @@ def load_image(url):
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
 
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title("Картинка с котиками")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.pack()
         label.image = img
 
 
@@ -35,22 +39,19 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
 
-label = Label()
-label.pack()
 
 # update_button = Button(text="Обновить", command=set_image)
 # update_button.pack()
 
 url = "https://cataas.com/cat"
 
-set_image()
 
 menu_bar = Menu(window) # меню в окне виндов
 window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0) # tearoff=0 чтобы меню не отклеивалось
 menu_bar.add_cascade(label='Файл', menu=file_menu)
-file_menu.add_command(label='Загрузить фото', command=set_image)
+file_menu.add_command(label='Загрузить фото', command=open_new_window)
 # file_menu.add_command(label='Сохранить изображение', command=save_image)
 file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
