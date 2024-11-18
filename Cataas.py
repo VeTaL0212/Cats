793d4f57e40dc5a0e642bc602ae755813af648c1
Vitali <vitaly.u@mail.ru>
2024-11-18 19:07:25 +0300

    Создано окно и виджет

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,23 @@
+from cProfile import label
 from tkinter import *
+from PIL import ImageTk
+import requests
+from io import BytesIO
+
+from pygame.examples.aliens import load_image
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
