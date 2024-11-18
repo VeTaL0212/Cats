4aea73ec25f010a82eece16f655b316c5b6c713c
Vitali <vitaly.u@mail.ru>
2024-11-18 21:23:32 +0300

    Добавлен combobox для выбора тэгов, поле entry удалено

--- Cataas.py
+++ Cataas.py
@@ -1,10 +1,11 @@
+from cProfile import label
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from pygame.display import update
-
+Allowed_tags = ["sleep", "jump", "fight", "black", "white", "bengal", "siamese", "cute"]
 
 def load_image(url):
     try:
@@ -20,7 +21,7 @@ def load_image(url):
 
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
     img = load_image(url_tag)
 
@@ -41,16 +42,6 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
 
-tag_entry = Entry()
-tag_entry.pack()
-
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
-load_button.pack()
-
-
-url = "https://cataas.com/cat"
-
-
 menu_bar = Menu(window) # меню в окне виндов
 window.config(menu=menu_bar)
 
@@ -60,5 +51,16 @@ file_menu.add_command(label='Загрузить фото', command=open_new_wind
 # file_menu.add_command(label='Сохранить изображение', command=save_image)
 file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
+
+url = "https://cataas.com/cat"
+
+tag_label = Label(text="Выбери тэг")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 window.mainloop()
